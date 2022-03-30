af260361a5ce6e651db5a32b38c6ce149a49537c
Daniel Camacho <dcamacho4@saddleback.edu>
2022-03-29 17:05:31 -0700

    Bug fix for window crash.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+// Constructor for MainWindow.
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
     restModel = new QSqlQueryModel;
@@ -10,12 +11,14 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
     on_checkBox_stateChanged(0);
 }
 
+// Destructor for MainWindow.
 MainWindow::~MainWindow() {
     delete ui;
     delete restModel;
     delete menuModel;
 }
 
+// Message received from successful admin login, launches administrative menu.
 void MainWindow::receiveMessage(const QString &msg) {
     hide();
     delete ui;
@@ -23,6 +26,7 @@ void MainWindow::receiveMessage(const QString &msg) {
     debugMenu->show();
 }
 
+// Update restaurant tableview and cart quanity.
 void MainWindow::restTableViewUpdate(int arg1) {
     QString sort = arg1 ? "d0" : "restName";
     restModel->setQuery("SELECT restName, d0 FROM restaurant ORDER BY " + sort);
@@ -32,10 +36,12 @@ void MainWindow::restTableViewUpdate(int arg1) {
     if (ui->cartQuantity->text() == "") ui->cartQuantity->setText("0");
 }
 
+// Order By Distance Checkbox: Change restaurant order from alphabetical order to distance from saddleback.
 void MainWindow::on_checkBox_stateChanged(int arg1) {
     restTableViewUpdate(arg1);
 }
 
+// Loads menu tableview when restaurant is selected from restaurant tableview.
 void MainWindow::on_rest_tableView_clicked(const QModelIndex &index) {
     menuItem = "";
     restName = index.siblingAtColumn(0).data().toString();
@@ -43,18 +49,16 @@ void MainWindow::on_rest_tableView_clicked(const QModelIndex &index) {
     ui->menu_tableView->setModel(menuModel);
 }
 
+// Login Window: Loads login window when login is selected from dropdown.
 void MainWindow::on_actionLogin_triggered() {
     login = new Login(this);
     login->show();
 }
 
+// Add Button: Adds selected menu item and quantity to cart database.
 void MainWindow::on_pushButton_4_clicked() {
-    if (menuItem == "") {
-        QMessageBox messageBox;
-        messageBox.critical(0,"Missing Menu Item", "Menu item must be selected");
-        messageBox.setFixedSize(500,200);
-        return;
-    }
+    if (menuItem == "") return;
+
     int quantity = ui->spinBox->text().toInt();
     QString restNum = "";
 
@@ -65,12 +69,7 @@ void MainWindow::on_pushButton_4_clicked() {
     query.exec("SELECT quantity FROM cart where restName =\"" + restName + "\" AND menuItem =\"" + menuItem + "\";");
     if (query.next()) {
         quantity += query.value(0).toInt();
-        if (quantity > 100) {
-            QMessageBox messageBox;
-            messageBox.critical(0,"Quantity Limit", "Menu item quantity cannot exceed 100");
-            messageBox.setFixedSize(500,200);
-            return;
-        }
+        if (quantity > 100) return;
         q = "UPDATE cart SET restName =\"" + restName + "\", menuItem =\"" + menuItem + "\", menuPrice =\"" + menuPrice + "\", quantity =\"" + QString::number(quantity) + "\" WHERE restName =\"" + restName + "\" AND menuItem =\"" + menuItem + "\";";
     }
     query.exec(q);
@@ -78,11 +77,13 @@ void MainWindow::on_pushButton_4_clicked() {
     if (query.next()) ui->cartQuantity->setText(query.value(0).toString());
 }
 
+// Updates menuItem and menuPrice when a menu item is selected from menu tableview
 void MainWindow::on_menu_tableView_clicked(const QModelIndex &index) {
     menuItem = index.siblingAtColumn(0).data().toString();
     menuPrice = index.siblingAtColumn(1).data().toString();
 }
 
+// Shopping Cart: Opens shopping cart window and closes main window.
 void MainWindow::on_cartButton_clicked() {
     shoppingCart = new ShoppingCart(this);
     shoppingCart->show();
@@ -90,7 +91,8 @@ void MainWindow::on_cartButton_clicked() {
     delete ui;
 }
 
-void MainWindow::on_pushButton_clicked() { // initial list trip
+// Initial List Button: Loads the custom trip with the initial list populated.
+void MainWindow::on_pushButton_clicked() {
     QSqlQuery query1("DROP TABLE trip;");
     QSqlQuery query2("CREATE TABLE trip (restNum INTEGER);");
     query1.exec("SELECT restNum FROM restaurant ORDER BY restNum LIMIT 10;");
@@ -101,13 +103,15 @@ void MainWindow::on_pushButton_clicked() { // initial list trip
     customTrip->show();
 }
 
-void MainWindow::on_pushButton_3_clicked() { // custom trip
+// Custom Trip Button: Loads the custom trip with no parameters loaded.
+void MainWindow::on_pushButton_3_clicked() {
     hide();
     delete ui;
     customTrip = new CustomTrip(this);
     customTrip->show();
 }
 
+// Quit Option: Closes the program when the Quit option is selected in the dropdown.
 void MainWindow::on_actionQuit_triggered() {
      QApplication::quit();
 }
